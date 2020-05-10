f4835b35e8f66637c1d19794074c51a0d7b5133a
Alex <alex.sulea@gmail.com>
2020-05-10 23:34:33 +0100

    added frontend to backend and back to front end communication

--- web_app/index/events.py
+++ web_app/index/events.py
@@ -25,6 +25,7 @@ def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
     room = session.get('room')
+    print("06848055885", room)
     emit('message', {'msg': session.get('name') + ':' + message['msg']})
 
 
--- web_app/index/views.py
+++ web_app/index/views.py
@@ -1,5 +1,8 @@
 from flask import session, redirect, url_for, render_template, request
 from . import index_blueprint
+# generate random integer values
+from random import seed
+from random import randint
 from .forms import LoginForm
 import yfinance as yf
 
@@ -7,7 +10,8 @@ import yfinance as yf
 @index_blueprint.route('/', methods=['GET', 'POST'])
 def index():
     print('---------')
-    return render_template('index.html')
+    room = randint(0, 10)
+    return render_template('index.html', room=room)
 
 
 

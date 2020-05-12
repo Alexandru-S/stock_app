6ed8a715c3c4bf17078637280ae4c69a2c534455
Alex <alex.sulea@gmail.com>
2020-05-12 01:28:43 +0100

    fetching ticker historical data

--- web_app/index/events.py
+++ web_app/index/events.py
@@ -4,47 +4,17 @@ from .. import socketio
 from pandas_datareader import data as pdr
 import yfinance as yf
 yf.pdr_override()
-import json
 
-@socketio.on('message')
-def handle_message(message):
-    print("MMMEE================")
-    print("-±§"*21)
-    print('received message: ' + message)
 
 @socketio.on('joined', namespace='/chat')
 def joined(message):
-    """Sent by clients when they enter a room.
-    A status message is broadcast to all people in the room."""
     room = session.get('room')
-    print("jpined rooom ====", room )
     join_room(room)
     emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
 
 
 @socketio.on('text', namespace='/chat')
 def text(message):
-    """Sent by a client when the user entered a new message.
-    The message is sent to all people in the room."""
-    print("|TEXTTTTTTTTT|", message)
     room = session.get('room')
-    ticker = pdr.get_data_yahoo(message["msg"])
-
-    print('ticker', type(ticker))
-    print("06848055885", room)
-    emit('message', {'msg': session.get('name') + ':' + ticker}, room=room)
-
-
-@socketio.on('something', namespace='nothing')
-def something(message):
-    print('=========', message)
-
-
-@socketio.on('left', namespace='/chat')
-def left(message):
-    print("LEEEEEFFFTTTTTTTTTTTT")
-    """Sent by clients when they leave a room.
-    A status message is broadcast to all people in the room."""
-    room = session.get('room')
-    leave_room(room)
-    emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
+    ticker = pdr.get_data_yahoo(message["msg"],start="2017-01-01", end="2017-04-30")
+    emit('message', {'msg': ticker.to_json(orient='table')}, room=room)

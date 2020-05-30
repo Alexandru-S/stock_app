63d071790b06250e30e6c5124cb601413acc5420
Alex <alex.sulea@gmail.com>
2020-05-30 23:44:32 +0100

    added css and js pages

--- web_app/index/__init__.py
+++ web_app/index/__init__.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
 
-index_blueprint = Blueprint('index', __name__, template_folder='templates', static_folder='static', static_url_path='/static')
+index_blueprint = Blueprint('index',
+                            __name__,
+                            template_folder='templates',
+                            static_folder='static',
+                            static_url_path='/index.static')
 
 from . import views, events

6ffc67267ccd881fe684185ea5dacabeb27c6706
Alex <alex.sulea@gmail.com>
2020-05-30 23:17:24 +0100

    fixed html spacing

--- web_app/index/__init__.py
+++ web_app/index/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-index_blueprint = Blueprint('index', __name__, template_folder='templates', static_folder='static')
+index_blueprint = Blueprint('index', __name__, template_folder='templates', static_folder='static', static_url_path='/static')
 
 from . import views, events

11246ec72e722f5d33722620b0e8b37dc39d07ea
Alexandru-S <alex.sulea@gmail.com>
2020-11-14 23:24:31 +0000

    added first part 0f data gatherring

--- web_app/index/views.py
+++
@@ -1,9 +0,0 @@
-from flask import url_for, render_template, request
-from . import index_blueprint
-from random import randint
-
-
-@index_blueprint.route('/', methods=['GET', 'POST'])
-def index():
-    room = randint(0, 10)
-    return render_template('index.html', room=room)

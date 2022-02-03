935f8b204593ee39feac42726d94c7e74670053f
Alexandru-S <Alexandru-S@users.noreply.github.com>
2022-02-03 00:21:57 +0000

    new app

--- web_app/index/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-index_blueprint = Blueprint('index',
-                            __name__,
-                            template_folder='templates',
-                            static_folder='static',
-                            static_url_path='/index.static')
-
-from . import views, events
